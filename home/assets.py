ec6c38eb8d33211e18f20e6694ce4e0fd2943ad9
Andrei Nicholson <contact@andreinicholson.com>
2012-05-14 19:05:51 -0600

    Normalize.css 99b187d1a2

--- home/assets.py
+++ home/assets.py
@@ -1,4 +1,4 @@
 from django_assets import Bundle, register
 
-register('css_all', Bundle('css/base.css',
+register('css_all', Bundle('css/normalize.css', 'css/base.css',
                            filters='cssutils', output='content.css'))
