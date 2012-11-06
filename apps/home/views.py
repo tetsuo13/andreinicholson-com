13368b40604e361943c9af8d39f72b8751e6fa32
Andrei Nicholson <contact@andreinicholson.com>
2012-11-05 21:07:17 -0700

    Finally just link to Instagram profile

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,30 +1,8 @@
 from django.http import HttpResponse
 from django.template import Context, loader, RequestContext
 
-from random import choice
-
 def index(request):
-    instagram_links = [
-        'http://instagr.am/p/azDxS/',
-        'http://instagr.am/p/cmgaZ/',
-        'http://instagr.am/p/cml3y/',
-        'http://instagr.am/p/cvTrP/',
-        'http://instagr.am/p/G7Bh1/',
-        'http://instagr.am/p/HlCpW/',
-        'http://instagr.am/p/hSP_R/',
-        'http://instagr.am/p/Hy7S0/',
-        'http://instagr.am/p/J_a6d/',
-        'http://instagr.am/p/JvDu0/',
-        'http://instagr.am/p/KH35z/',
-        'http://instagr.am/p/KRHvL/',
-        'http://instagr.am/p/KLmWp/',
-        'http://instagr.am/p/Kztpg/',
-        'http://instagr.am/p/X__k_/',
-        'http://instagr.am/p/Zgi5n/'
-    ]
-
     dictionary = {
-        'instagram_link': choice(instagram_links),
         'current_nav': 'home'
     }
 
